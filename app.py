8bd8c4e5a5d6b7682b7706c64c9f276ac3fafa09
AsbjornL <a.bjorn.2nd@gmail.com>
2024-06-08 14:51:20 +0200

    Upper and lower bound

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import psycopg2
 
 app = Flask(__name__)
 
 # set your own database
-db = "dbname='test' user='postgres' host='127.0.0.1' password = '123'"
+db = "dbname='postgres' user='postgres' host='127.0.0.1' password = '123'"
 conn = psycopg2.connect(db)
 
 @app.route('/')
@@ -13,33 +13,36 @@ def hello():
 
 def select_problems(tags, rating_lower_bound, rating_upper_bound):
     cur = conn.cursor()
-    sql = "SELECT id, name, rating FROM problems"
+    sql = "SELECT name, id, rating FROM problems"
     constraints = []
     if rating_upper_bound:
         constraints.append(f"rating <= {rating_upper_bound}")
     if rating_lower_bound:
         constraints.append(f"rating >= {rating_lower_bound}")
     if tags:
-        con = "tag IN (" + ', '.join(f"\'{tag}\'" for tag in tags) + f") GROUP BY id HAVING COUNT (DISTINCT tag) = {len(tags)}"
+        con = "tag IN (" + ', '.join(f"\'{tag}\'" for tag in tags) + f") GROUP BY name, id, rating HAVING COUNT (DISTINCT tag) = {len(tags)}"
         constraints.append(con)
     if constraints:
         sql += " WHERE " + " AND ".join(constraints)
     print(sql)
     cur.execute(sql)
-    problems = Problem(cur.fetchall())
+    result = cur.fetchall()
+    problems = [Problem(problem) for problem in result]
     cur.close()
 
     return problems
 
 class Problem:
-    def __init__(self, id, rating, name):
-        self.name = name
-        self.rating = rating
-        self.id = id
+    def __init__(self, data):
+        self.name = data[0]
+        self.id = data[1]
+        self.rating = data[2]
 
-@app.route('/problems/')
+@app.route('/problems/', methods=['GET'])
 def problem_list():
-    problems = select_problems(["fft", "binary search"], 3000, 3500)
+    rating_lower_bound = request.args.get('rating_lower_bound', type=int)
+    rating_upper_bound = request.args.get('rating_upper_bound', type=int)
+    problems = select_problems(["bitmasks", "greedy"], rating_lower_bound, rating_upper_bound)
     return render_template('index.html', problems=problems)
 
 if __name__ == "__main__":
